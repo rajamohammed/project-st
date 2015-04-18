fb614d218cdd7821aca40a14907f09379e716d40
rajamohammed <maks.raja@gmail.com>
2015-04-19 00:50:09 +0530

    Todo Appa
    Elementary - Working app -

---
+++ todo.js
@@ -0,0 +1,163 @@
+ 
+	var error = '';
+	var display = document.getElementById('listview');
+	var currentdate = new Date(); 
+	var datetime =  currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/" 
+                + currentdate.getFullYear() + " @ "  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds();
+
+	var todo_model = {
+		
+
+		newTodo : function(obj)
+		{
+			
+			 
+			 if(obj.item != '' )
+			 {
+			 		// data.push(JSON.parse(localStorage.todoitem));
+				 	// data.push(obj);
+				 	// localStorage.todoitem = JSON.stringify(data);
+				 	// data = [];
+			 	 // if(localStorage.length > 0)
+			 	 // {
+			 	 // 	data = JSON.parse(localStorage.getItem('todoitem'));
+				  // 	data.push(obj);
+				  // 	localStorage.setItem('todoitem' , JSON.stringify(data));
+			 	 // }
+			 	 // else
+			 	 // {
+			 	 // 	localStorage.setItem('todoitem' ,JSON.stringify(obj));
+			 	 // }
+			 	//  if(localStorage.getItem('todoitem') != '')
+			 	//  {
+			 	//  	var data = JSON.parse(localStorage.getItem('todoitem'));
+			 	//  	var datas = [data];
+			 	//  	datas.push(obj);
+					// localStorage.setItem('todoitem' , JSON.stringify(datas));
+			 	//  }
+			 	//  else
+			 	// {
+			 	// 	localStorage.setItem('todoitem' , JSON.stringify(obj));
+			 			
+			 	// }
+
+			 	var data = JSON.parse(localStorage.getItem('todoitem'));
+			 	var datas = new Array();
+			 	if(data!==null)
+			 	{
+			 		datas = data;
+			 		datas.push(obj);
+			 	}
+			 	else
+			 	{
+			 		datas.push(obj);
+			 	}
+			 	
+			 	
+				localStorage.setItem('todoitem' , JSON.stringify(datas));
+ 
+			 }
+			 else
+			 {
+			 	error = 'Empty Item couldnot be included';
+			 }
+			 controller.init();
+
+		},
+
+		getItems : function()
+		{
+			return JSON.parse(localStorage.getItem('todoitem'));
+		},
+
+		storageLen : function()
+		{
+			return localStorage.getItem('todoitem');
+		},
+
+		init : function(){
+			if(!localStorage.getItem('todoitem'))
+			{
+				var values = [];
+				localStorage.setItem('todoitem',values);
+			}
+		}
+		
+
+	};
+
+
+	var controller = {
+
+		
+		addNew : function(todoVal)
+		{
+			var sendtoModel = {
+				item : todoVal,
+				date  : datetime,
+				status : 1
+			};
+			
+			todo_model.newTodo(sendtoModel);
+		},
+
+		getList : function()
+		{
+			return todo_model.getItems();
+		},
+
+		getStorageLen : function()
+		{
+			return todo_model.storageLen();
+		},
+
+		init : function(){
+			view.init();
+			todo_model.init();
+		}
+	};
+
+
+	
+
+	var view = {
+		
+		init : function(){
+			document.getElementById('submit').addEventListener('click',function(){
+				var todoVal = document.getElementById('todo').value;
+				controller.addNew(todoVal);
+			});
+			this.renderView();
+		},
+		
+		renderView : function()
+		{	 
+				 var html = '';
+			    if(controller.getStorageLen() != '')
+			    {
+			  	   	controller.getList().forEach(function(todoitem){
+					 	 html += '<li>'+todoitem.item+'<br>'+todoitem.date+'</li>';
+					});
+ 					//listhis = JSON.parse(controller.getList());
+ 					//for(todoitem in listhis){
+					//	 html += '<li>'+listhis[todoitem] +'</li>';
+					//};
+					display.innerHTML =  html;
+			    }
+				
+		}
+
+	};
+
+
+	
+
+
+
+
+ 
+
